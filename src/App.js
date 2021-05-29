15c44d483ab7d7b2cf66f4b8f725e02913124d37
VaibhavTheVicar <Vaibhavtiwari.ece17@nituk.ac.in>
2021-05-29 17:07:35 +0530

    logic for handling clicks layed out

--- src/App.js
+++ src/App.js
@@ -3,42 +3,65 @@ export default function App() {
 	const questions = [
 		{
 			questionText: 'What is the time complexity for quick sort',
+			negation: true,
 			answerOptions: [
-				{ answerText: 'nlogn', isCorrect: true },
-				{ answerText: 'logn', isCorrect: false },
-				{ answerText: 'n^2', isCorrect: false },
-				{ answerText: 'nlog*n', isCorrect: false },
+				{ answerText: 'nlogn', verdict: true },
+				{ answerText: 'logn', verdict: false },
+				{ answerText: 'n^2', verdict: false },
+				{ answerText: 'nlog*n', verdict: false },
 			],
 		},
 		{
 			questionText: 'Which engine does node.js rip off?',
+			negation: true,
 			answerOptions: [
-				{ answerText: 'Safari 3', isCorrect: false },
-				{ answerText: 'Chrome V8', isCorrect: true },
-				{ answerText: 'FireFox v3', isCorrect: false },
+				{ answerText: 'Safari 3', verdict: false },
+				{ answerText: 'Chrome V8', verdict: true },
+				{ answerText: 'FireFox v3', verdict: false },
 			],
 		},
 		{
 			questionText: 'The language used to vreate iOS apps mainly is?',
+			negation: true,
 			answerOptions: [
-				{ answerText: 'Swift', isCorrect: true },
-				{ answerText: 'Java', isCorrect: false },
-				{ answerText: 'JavaScript', isCorrect: false },
-				{ answerText: 'Visual Basic', isCorrect: false },
+				{ answerText: 'Swift', verdict: true },
+				{ answerText: 'Java', verdict: false },
+				{ answerText: 'JavaScript', verdict: false },
+				{ answerText: 'Visual Basic', verdict: false },
 			],
 		},
 		{
 			questionText: 'Who made the linux OS?',
+			negation: true,
 			answerOptions: [
-				{ answerText: 'Linus Trivaldus', isCorrect: false },
-				{ answerText: 'Steve Jobs', isCorrect: false },
-				{ answerText: 'Henry Cohern', isCorrect: false },
+				{ answerText: 'Linus Trivaldus', verdict: false },
+				{ answerText: 'Steve Jobs', verdict: false },
+				{ answerText: 'Henry Cohern', verdict: false },
 			],
 		},
 	];
 	const [displayedQuestion, changeDisplayedQuestion] = useState(0);
-	const [testOver, isTestOver] = useState(false);
-	const[gotRight,increaseGotRight] = useState(0);
+	const [testOver, setTestOver] = useState(false);
+	const[score, changeScore] = useState(0);
+	
+	const handleScreenState = (verdict,negation) => {
+		if(verdict){
+			changeScore(score+1);
+		}
+		else{
+			if(negation){
+				changeScore(score-1);
+			}
+		}
+
+		if(currentQuestion+1<questions.length){
+			changeDisplayedQuestion(currentQuestion+1)
+		}
+		else{
+			setTestOver(true)
+		}
+	}
+	
 	return (
 		<h1>Hello World!</h1>
 	);
