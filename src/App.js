438c4362372766bc88e245084173ae9442afb162
VaibhavTheVicar <Vaibhavtiwari.ece17@nituk.ac.in>
2021-05-29 16:55:18 +0530

    hard coded questions for now

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { useState } from 'react';
+export default function App() {
+	const questions = [
+		{
+			questionText: 'What is the time complexity for quick sort',
+			answerOptions: [
+				{ answerText: 'nlogn', isCorrect: true },
+				{ answerText: 'logn', isCorrect: false },
+				{ answerText: 'n^2', isCorrect: false },
+				{ answerText: 'nlog*n', isCorrect: false },
+			],
+		},
+		{
+			questionText: 'Which engine does node.js rip off?',
+			answerOptions: [
+				{ answerText: 'Safari 3', isCorrect: false },
+				{ answerText: 'Chrome V8', isCorrect: true },
+				{ answerText: 'FireFox v3', isCorrect: false },
+			],
+		},
+		{
+			questionText: 'The language used to vreate iOS apps mainly is?',
+			answerOptions: [
+				{ answerText: 'Swift', isCorrect: true },
+				{ answerText: 'Java', isCorrect: false },
+				{ answerText: 'JavaScript', isCorrect: false },
+				{ answerText: 'Visual Basic', isCorrect: false },
+			],
+		},
+		{
+			questionText: 'Who made the linux OS?',
+			answerOptions: [
+				{ answerText: 'Linus Trivaldus', isCorrect: false },
+				{ answerText: 'Steve Jobs', isCorrect: false },
+				{ answerText: 'Henry Cohern', isCorrect: false },
+			],
+		},
+	];
+	const [displayedQuestion, changeDisplayedQuestion] = useState(0);
+	const [testOver, isTestOver] = useState(false);
+	const[gotRight,increaseGotRight] = useState(0);
+	return (
+		<h1>Hello World!</h1>
+	);
 }
-
-export default App;
